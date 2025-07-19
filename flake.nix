80e96a5f06c98e8e63c2e8316ddb3328a8ebf825
Tymofiy Saukh <130227991+imnottimaq@users.noreply.github.com>
2025-07-19 13:56:45 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "MT7601U kernel driver patch for AP mode";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem
