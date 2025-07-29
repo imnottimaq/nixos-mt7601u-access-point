8c699986b6fc7820ce4a16301e70e7905d487d98
Tymofiy Saukh <tima3050505@gmail.com>
2025-07-30 01:19:04 +0300

    just hardcoded nixpkgs version

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "MT7601U kernel driver patch for AP mode";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
   outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem
